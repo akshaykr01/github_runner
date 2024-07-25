817174e9efc60219485cce1770c80c7379cf2da5
akshaykr01 <74856114+akshaykr01@users.noreply.github.com>
2024-07-25 14:28:06 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # exit immediately if a command exits with a non-zero status
 set -e
 
-# ensure the required environment variables are set AR3DNMVJ62ESRPB27JIEZLLGT6PEY-github-token, https://github.com/akshaykr01/node-app - github-url
+# ensure the required environment variables are set -github-token, https://github.com/akshaykr01/node-app - github-url
 if [ -z "$GH_RUNNER_TOKEN" ] || [ -z "$GH_RUNNER_URL" ]; then
   echo "$GH_RUNNER_TOKEN and $GH_RUNNER_URL environment variables must be set"
   exit 1
